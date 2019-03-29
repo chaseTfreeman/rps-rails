2f5489917ba3fc40361bb5de908f564d5f54e658
Chase Freeman <chase.freeman@msi-inc.com>
2019-03-29 08:38:16 -0700

    basic game working need to style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def respond_modal_with(*args, &blk)
+      options = args.extract_options!
+      options[:responder] = ModalResponder
+      respond_with *args, options, &blk
+    end
+    
 end
--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -4,9 +4,22 @@ class RequestController < ActionController::Base
   def curb_throw
     response = HTTParty.get('https://5eddt4q9dk.execute-api.us-east-1.amazonaws.com/rps-stage/throw')
     downcased_response = response["body"].gsub('"','').downcase
-    puts params[:player_throw]
-    puts downcased_response
-    # render json: response
+    player_throw = params[:player_throw]
+    curb_throw = downcased_response
+    puts curb_throw
+
+    wins = {'scissors' => 'paper', 'rock' => 'scissors', 'paper' => 'rock'}
+puts wins
+puts curb_throw
+    if curb_throw == player_throw
+      render :template => "requests/draw"
+    elsif wins[curb_throw] == player_throw
+      render :template => "requests/lose"
+    elsif wins.key?(curb_throw) === false
+      render :template => "requests/error"
+    else
+      render :template => "requests/win"
+    end
   end
 
 end
